360f0a40fbbb08ece9200ce697d736232425fe11
Hesham Abbod <hshm_aboud@yahoo.com>
2020-12-17 17:43:20 +0300

    Update settings.py

--- jobsApp/settings.py
+++ jobsApp/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
