e03951ba111d2aa7550e883a1069982cae5634a5
Hesham Abbod <hshm_aboud@yahoo.com>
2020-12-17 20:23:20 +0300

    Update settings.py

--- jobsApp/settings.py
+++ jobsApp/settings.py
@@ -12,7 +12,7 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, '/app/static/')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PROJECT_ROOT)
